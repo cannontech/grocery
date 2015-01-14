5cab70e8606885309f6c4427efc440aadeed1901
eschmit <eschmit@groceryshopping.net>
2015-01-14 12:46:34 -0600

    configuration for dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,12 @@
  # The project repo name. (one word: no spaces, dashes, or underscores)
-set :application, "test" #FIXME
+set :application, "grocery" #FIXME
 
 # Legacy from porting from drupal, best to just leave this as "default"
 set :domains, ["default"]
 
 # Set the repository type and location to deploy from.
 set :scm, :git
-set :repository,  "git@github.com:kronda/wp_capistrano.git" #FIXME
-# set :scm, :subversion
-# set :repository,  "https://svn.example.com/svn/#{application}/trunk/"
-# set(:scm_password) { Capistrano::CLI.password_prompt("SCM Password: ") }
+set :repository,  "git@github.com:cannontech/grocery.git" #FIXME
 
 # Set the database passwords that we'll use for maintenance. Probably only used
 # during setup.
@@ -17,12 +14,12 @@ set(:db_root_pass) { Capistrano::CLI.password_prompt("Production Root MySQL pass
 set(:db_pass) { random_password }
 
 # The subdirectory within the repo containing the DocumentRoot.
-set :app_root, "webroot"
-set :local_path, "/Path/to/your/local-folder" #FIXME
+set :app_root, "wordpress"
+set :local_path, "C:\Code\grocery" #FIXME
 
 # Use a remote cache to speed things up
 set :deploy_via, :remote_cache
-ssh_options[:user] = 'username' #FIXME
+ssh_options[:user] = 'ubuntu@54.193.16.84' #FIXME
 
 # Multistage support - see config/deploy/[STAGE].rb for specific configs
 set :default_stage, "dev"
@@ -35,12 +32,8 @@ set :use_sudo, false
 default_run_options[:pty] = true
 
 # Override these in your stage files if your web server group is something other than apache
-set :httpd_group, 'apache'
+set :httpd_group, 'www-data'
 
 
 #set this to where you have installed your wp site locally (http://[local_domain]/wp-admin/ should exist. )
-<<<<<<< HEAD
-set :local_domain, 'localhost.com' #FIXME
-=======
-set :local_domain, 'local.domain.com'
->>>>>>> 4201a433b092ad8658bc1e117f1b2ba6fdc2c0ad
+set :local_domain, 'grocery.com' #FIXME
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,19 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
+set :deploy_to, "/mnt/sharefs/#{application}-#{stage}" #FIXME
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
-set :branch, "dev" #FIXME
+#set :branch, "dev" #FIXME
+set :branch, "master" #FIXME
 
 # The hostnames to deploy to.
-role :web, "yourdomain.com" #FIXME
+role :web, "grocery.com" #FIXME
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "yourdomain.com", :primary => true #FIXME
+role :db, "grocery.com", :primary => true #FIXME
 
 # The path to wp-cli
-set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
+#set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'username' #FIXME
+#ssh_options[:user] = 'username' #FIXME
